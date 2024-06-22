56ddf62a9f1f996da3ccac5a49383197fa545bc7
Arpan <64348757+MeMasterMind@users.noreply.github.com>
2024-06-22 13:52:17 +0530

    styling

--- src/config.js
+++ src/config.js
@@ -2,7 +2,8 @@ let form = document.getElementById("search");
 form.addEventListener('submit', googleSearch);
 
 
-function googleSearch(){
+function googleSearch(e){
+    e.preventDefault();
     let searchElement = document.getElementById("searchBox")
     var query = searchElement.value;
     window.location.href = "https://www.google.com/search?q=" + query;
